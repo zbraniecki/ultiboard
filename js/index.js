919eec849e45b499ddf2d2c85adfcfa53af6791b
Zbigniew Braniecki <gandalf@mozilla.com>
2012-10-03 17:21:21 +0200

    rework the json file to allow for scenarios and key frames

--- js/index.js
+++ js/index.js
@@ -47,6 +47,10 @@ function setupField() {
 var setups = {}
 
 function calculate_pos(pos, rotate) {
+  if (pos['pos']) {
+    pos = pos['pos'];
+  }
+
   var field = settings['field'];
   var circle = settings['circle'];
   var scale = settings['field']['scale'];
@@ -88,15 +92,67 @@ function position_players(play) {
 
 function load_plays() {
   $.getJSON(
-    './4hands.json',
+    './4hands2.json',
     function(data) {
       setups = data;
-      for (var i in data) {
+      var types = {};
+      var type = null;
+      var play = null;
+      var scenario = null;
+      var sid = null;
+      var kf = null;
+      var kfid = null;
+
+      for (var i=0;i<data['plays'].length;i++) {
+        var p = data['plays'][i];
+        if (!types[p['type']]) {
+          types[p['type']] = {};
+        }
+        if (type === null) {
+          type = p['type'];
+        }
+        types[p['type']][p['name']] = p;
+      }
+      for (var i in types) {
+        $("<option/>").attr('value', i).text(i).appendTo($("#types"));
+      }
+      
+      for (var i in types[type]) {
+        if (play === null) {
+          play = i;
+        }
         $("<option/>").attr('value', i).text(i).appendTo($("#plays"));
       }
-      position_players(setups[settings['play']]);
+      for (var i in types[type][play]['scenarios']) {
+        var s = types[type][play]['scenarios'][i];
+        if (scenario === null) {
+          scenario = s;
+          sid = i;
+        }
+        $("<option/>").attr('value', i).text(s['desc']).appendTo($("#scenarios"));
+      }
+      for (var i in types[type][play]['scenarios'][sid]['kfs']) {
+        var k = types[type][play]['scenarios'][sid]['kfs'][i];
+        if (kf === null) {
+          kf = k;
+          kfid = i;
+        }
+        $("<option/>").attr('value', i).text(k['desc']).appendTo($("#kfs"));
+      }
+      position_players(types[type][play]['scenarios'][sid]['kfs'][kfid]);
     }
   );
+}
+
+function draw_vision(player) {
+  var pos = $(".player"+player).position();
+  var setup = setups[settings['play']];
+  var vis = setup['home'][player-1]['vis'];
+  if (vis) {
+    var d = $("<div/>").attr('id', 'vision').appendTo($(".field"));
+    d.css({'top': pos['top']-45+10, 'left': pos['left']-45+10});
+    d.css('-moz-transform', 'rotate('+vis+'deg)');
+  }
 }
 
 function init() {
@@ -106,6 +162,12 @@ function init() {
   setupField();
   $(".field").show();
   load_plays();
+}
+
+function switch_fov(f) {
+  for(var i=0;i<7;i++) {
+    draw_vision(i+1);
+  }
 }
 
 function switch_mode(t) {
